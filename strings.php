16846a5956743a6054668c85a6f7acf514874eb4
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-07-01 22:34:08 +0200

    dryzzato

--- strings.php
+++ strings.php
@@ -1,23 +1,17 @@
 <?php
 
-
 function str_replace_marker($a, $s, $b, $e) {
   preg_match_all('/' . preg_quote($b) . '(.*?)' . preg_quote($e) . '/', $s, $g, PREG_OFFSET_CAPTURE);
 
-  $e = [];
   $k = array_keys($a);
   $k1 = array_column($g[1], 0);
 
-  if (($d = array_diff($k1, $k)) !== []) {
-    $e[] = "Mancano le sostituzioni per i seguenti marcatori:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d));
-  }
-  if (($d = array_diff($k, $k1)) !== []) {
-    $e[] = "Vengono rimpiazzati dei marcatori inesistenti:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d));
-  }
-  if (($d = array_keys(array_filter($a, fn ($a) => gettype($a) !== 'string'))) !== []) {
-    $e[] = "Le sostituzioni non sono stringhe:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d));
-  }
-  if ($e) {
+  $c = fn ($d, $m) => count($d) === 0 ? false : "$m:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d));
+  if ($e = array_filter([
+    $c(array_diff($k1, $k), "Mancano le sostituzioni per i seguenti marcatori"),
+    $c(array_diff($k, $k1), "Vengono rimpiazzati dei marcatori inesistenti"),
+    $c(array_keys(array_filter($a, fn ($a) => gettype($a) !== 'string')), "Le sostituzioni non sono stringhe"),
+  ])) {
     echo implode("\n", $e);
     die(1);
   }
