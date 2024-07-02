26fd933da144a240880300e589d5029a17f61caa
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-07-02 10:36:21 +0200

    codice di errore utile per i testing

--- strings.php
+++ strings.php
@@ -6,16 +6,15 @@ function str_replace_marker($s, $a, $b, $e) {
   $k = array_keys($a);
   $k1 = array_column($g[1], 0);
 
-  $c = fn ($d, $m) => count($d) === 0 ? false : "$m:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d));
+  $c = fn ($e, $d, $m) => count($d) === 0 ? false : [$e, "$m:\n" . implode('', array_map(fn ($a) => "\t$a\n", $d))];
   if ($e = array_filter([
-    $c(array_diff($k1, $k), "Mancano le sostituzioni per i seguenti marcatori"),
-    $c(array_diff($k, $k1), "Vengono rimpiazzati dei marcatori inesistenti"),
-    $c(array_keys(array_filter($a, fn ($a) => gettype($a) !== 'string')), "Le sostituzioni non sono stringhe"),
+    $c(1, array_diff($k1, $k), "Mancano le sostituzioni per i seguenti marcatori"),
+    $c(2, array_diff($k, $k1), "Vengono rimpiazzati dei marcatori inesistenti"),
+    $c(4, array_keys(array_filter($a, fn ($a) => gettype($a) !== 'string')), "Le sostituzioni non sono stringhe"),
   ])) {
-    $e = implode("\n", $e) . "\n\nTesto da rimpiazzare:\n\n$s";
-    //echo $e;
-    throw new ErrorException("$e\n\nstr_replace_marker_error");
-    // die(1);
+    $c = array_sum(array_column($e, 0));
+    $e = implode("\n", array_column($e, 1)) . "\n\nTesto da rimpiazzare:\n\n$s";
+    throw new ErrorException("$e\n\nstr_replace_marker_error", $c);
   }
 
   $g = array_map(fn ($i, $j) => [$i[0], $i[1], $j[0]], $g[0], $g[1]);
--- tests.php
+++ tests.php
@@ -3,21 +3,89 @@
 require_once __DIR__ . '/strings.php';
 
 //*
-function str_replace_marker_test($msg, $a, $s, $b, $e) {
-  echo "\n\n[$msg]" . implode('', array_map(fn () => '=', range(0, 67))) . "\n";
-  ob_start(fn ($a) => implode('', array_map(fn ($a) => "[$msg]: $a\n", explode("\n", $a))));
-  try {
-    echo str_replace_marker($s, $a, $b, $e);
-  } catch (Exception $e) {
-    echo $e->getMessage() . "\n";
-    echo $e->getTraceAsString();
-  }
-  ob_end_flush();
-  echo "[$msg]" . implode('', array_map(fn () => '=', range(0, 67))) . "\n\n";
+function test($m, $f, $a, $c) {
+  echo "[$f]: $m: " . ($c(fn () => $f(...$a)) ? 'OK' : 'ERR') . "\n";
 }
-str_replace_marker_test("corretto", ['a' => 'A', 'b' => 'B'], '{a}{b}c', '{', '}');
-str_replace_marker_test("marcatore mancante", ['a' => 'A'], '{a}{b}c', '{', '}');
-str_replace_marker_test("marcatore inesistente", ['a' => 'A', 'b' => 'B', 'c' => 'C'], '{a}{b}c', '{', '}');
-str_replace_marker_test("marcatore mancante e inesistente", ['a' => 'A', 'c' => 'C'], '{a}{b}c', '{', '}');
-str_replace_marker_test("marcatore non stringa", ['a' => 'A', 'b' => [0]], '{a}{b}c', '{', '}');
+
+test(
+  'Corretto',
+  'str_replace_marker',
+  [
+    '{a}{b}c',
+    ['a' => 'A', 'b' => 'B'],
+    '{', '}',
+  ],
+  function ($f) {
+    return $f() === 'ABc';
+  }
+);
+
+test(
+  'Marcatore mancante',
+  'str_replace_marker',
+  [
+    '{a}{b}c',
+    ['a' => 'A'],
+    '{', '}',
+  ],
+  function ($f) {
+    try {
+      $f();
+    } catch (ErrorException $e) {
+      return $e->getCode() & 1;
+    }
+  }
+);
+
+test(
+  'Marcatore inesistente',
+  'str_replace_marker',
+  [
+    '{a}{b}c',
+    ['a' => 'A', 'b' =>  'B', 'c' => 'C'],
+    '{', '}',
+  ],
+  function ($f) {
+    try {
+      $f();
+    } catch (ErrorException $e) {
+      return $e->getCode() & 2;
+    }
+  }
+);
+
+test(
+  'Marcatore mancante e inesistente',
+  'str_replace_marker',
+  [
+    '{a}{b}c',
+    ['a' => 'A', 'c' => 'C'],
+    '{', '}',
+  ],
+  function ($f) {
+    try {
+      $f();
+    } catch (ErrorException $e) {
+      return $e->getCode() & (1 | 2);
+    }
+  }
+);
+
+test(
+  'Marcatore non stringa',
+  'str_replace_marker',
+  [
+    '{a}{b}c',
+    ['a' => 'A', 'b' => [0]],
+    '{', '}',
+  ],
+  function ($f) {
+    try {
+      $f();
+    } catch (ErrorException $e) {
+      return $e->getCode() & 4;
+    }
+  }
+);
+
 //*/
