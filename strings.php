1c53787776de0b8bebc691742f87e381f339ee91
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-07-02 09:37:26 +0200

    migliore gestione dei test

--- strings.php
+++ strings.php
@@ -12,8 +12,10 @@ function str_replace_marker($s, $a, $b, $e) {
     $c(array_diff($k, $k1), "Vengono rimpiazzati dei marcatori inesistenti"),
     $c(array_keys(array_filter($a, fn ($a) => gettype($a) !== 'string')), "Le sostituzioni non sono stringhe"),
   ])) {
-    echo implode("\n", $e);
-    die(1);
+    $e = implode("\n", $e) . "\n\nTesto da rimpiazzare:\n\n$s";
+    //echo $e;
+    throw new ErrorException("$e\n\nstr_replace_marker_error");
+    // die(1);
   }
 
   $g = array_map(fn ($i, $j) => [$i[0], $i[1], $j[0]], $g[0], $g[1]);
@@ -26,18 +28,22 @@ function str_replace_marker($s, $a, $b, $e) {
   return $s;
 }
 
-/*
+//*
 function test($msg, $a, $s, $b, $e) {
-  echo "\n";
-  echo $msg;
-  echo "\n";
-  echo str_replace_marker($s, $a, $b, $e);
-  echo "\n";
-  echo "\n";
+  echo "\n\n[$msg]" . implode('', array_map(fn () => '=', range(0, 67))) . "\n";
+  ob_start(fn ($a) => implode('', array_map(fn ($a) => "[$msg]: $a\n", explode("\n", $a))));
+  try {
+    echo str_replace_marker($s, $a, $b, $e);
+  } catch (Exception $e) {
+    echo $e->getMessage() . "\n";
+    echo $e->getTraceAsString();
+  }
+  ob_end_flush();
+  echo "[$msg]" . implode('', array_map(fn () => '=', range(0, 67))) . "\n\n";
 }
-//test("corretto",['a' => 'A', 'b' => 'B'], '{a}{b}c', '{', '}');
-//test("marcatore mancante",['a' => 'A'], '{a}{b}c', '{', '}');
-//test("marcatore inesistente",['a' => 'A', 'b' => 'B', 'c' => 'C'], '{a}{b}c', '{', '}');
-//test("marcatore mancante e inesistente",['a' => 'A', 'c' => 'C'], '{a}{b}c', '{', '}');
-//test("marcatore non stringa",['a' => 'A', 'b' => [0]], '{a}{b}c', '{', '}');
+test("corretto", ['a' => 'A', 'b' => 'B'], '{a}{b}c', '{', '}');
+test("marcatore mancante", ['a' => 'A'], '{a}{b}c', '{', '}');
+test("marcatore inesistente", ['a' => 'A', 'b' => 'B', 'c' => 'C'], '{a}{b}c', '{', '}');
+test("marcatore mancante e inesistente", ['a' => 'A', 'c' => 'C'], '{a}{b}c', '{', '}');
+test("marcatore non stringa", ['a' => 'A', 'b' => [0]], '{a}{b}c', '{', '}');
 //*/
